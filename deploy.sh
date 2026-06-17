f06eae1f45ae6b88dc6ba2abd8725ec1d6cc2261
nba914 <119274624+nba914@users.noreply.github.com>
2026-06-17 05:49:49 +0000

    更新 自動化部署 deploy.sh 與 Docker 環境

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,13 @@ echo "網頁部署自動化啟動"
 echo "=================="
 
 echo "正在移除舊檔"
-sudo docker rm -f my-web-container 2>/dev/null
+docker rm -f my-web-container 2>/dev/null
 
 echo "正在更新ISO檔"
-sudo docker build -t my-web-image .
+docker build -t my-web-image .
 
 echo "正在更新伺服器"
-sudo docker run -d -p 80:80 --name my-web-container my-web-image
+docker run -d -p 80:80 --name my-web-container my-web-image
 
 echo "======================="
 echo "已成功更新 請刷新瀏覽器"
