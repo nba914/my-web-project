b1aab160349161183b1c08356aba6827976b165b
nba914 <119274624+nba914@users.noreply.github.com>
2026-06-21 16:59:59 +0800

    安全分離腳本更新

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-# 1. 透過 Docker 襪子（sock）直接擊殺外面的 Nginx 網頁貨櫃
+# 1. 擊殺舊的 Nginx 網頁貨櫃
 docker stop my-html-web || true
 docker rm my-html-web || true
 
-# 2. 安全搬運：從 Jenkins 自己的工作區，搬到剛剛開闢的內部門戶 /var/web_code
-#  這樣一搬，宿主機的實體 web_code 資料夾就會隔空同步收到檔案！
+# 2. 安全搬運：把工廠拉下來的東西，搬到對外的 /var/web_code
 cp -r /var/jenkins_home/workspace/dio-html-pipeline/* /var/web_code/
 
-# 3. 啟動 Nginx：讓 Nginx 抱住宿主機實體的 web_code
+# 3. 終極點火：讓 Nginx 用平民身分（1000）去聽它內部的 8080 埠！
+# 💡 這樣它既能跨進 700 的大門，又不會因為搶 80 埠而暴斃！
+# 💡 注意看連接埠映射變成了：-p 80:8080 (外面依然是80，裡面走8080)
 docker run -d \
   --name my-html-web \
   --user 1000:1000 \
-  -p 80:80 \
+  -p 80:8080 \
   -v /home/ubuntu/dio_company_center/web_code:/usr/share/nginx/html \
   nginx:alpine
