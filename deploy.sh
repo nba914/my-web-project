a3509ddbbd394088167bb22ef363484f546cf037
nba914 <119274624+nba914@users.noreply.github.com>
2026-06-21 16:29:07 +0800

    deploy.sh
    jenkins & html 分離

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# 1. 免死金牌：擊殺舊的網頁貨櫃（注意：不是擊殺 Jenkins 喔，是擊殺 Nginx 網頁貨櫃！）
+# 1. 擊殺舊的網頁貨櫃（注意：不是擊殺 Jenkins 喔，是擊殺 Nginx 網頁貨櫃！）
 docker stop my-html-web || true
 docker rm my-html-web || true
 
 # 2. 安全搬運：把 Jenkins 剛拉下來的最新網頁檔案，隔空複製到對外的安全專區
-# 💡 注意：請把底下的「dio-html-pipeline」改成你實際在 Jenkins 看到的專案資料夾名稱！
+# 注意：請把底下的「dio-html-pipeline」改成你實際在 Jenkins 看到的專案資料夾名稱！
 cp -r /var/jenkins_home/workspace/dio-html-pipeline/* /home/ubuntu/dio_company_center/web_code/
 
-# 3. 點火重生：讓 Nginx 網頁貨櫃死死掛載在「純淨的 web_code」
+# 3. 讓 Nginx 網頁貨櫃死死掛載在「純淨的 web_code」
 docker run -d \
   --name my-html-web \
   -p 80:80 \
