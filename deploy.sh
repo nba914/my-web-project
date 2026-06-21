13cee3ba0c30feb1ce4281ce550bb2299e0932d7
nba914 <119274624+nba914@users.noreply.github.com>
2026-06-21 16:38:05 +0800

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-
-# 1. 擊殺舊的網頁貨櫃（注意：不是擊殺 Jenkins 喔，是擊殺 Nginx 網頁貨櫃！）
+# 1. 透過 Docker 襪子（sock）直接擊殺外面的 Nginx 網頁貨櫃
 docker stop my-html-web || true
 docker rm my-html-web || true
 
-# 2. 安全搬運：把 Jenkins 剛拉下來的最新網頁檔案，隔空複製到對外的安全專區
-# 注意：請把底下的「dio-html-pipeline」改成你實際在 Jenkins 看到的專案資料夾名稱！
-cp -r /var/jenkins_home/workspace/dio-html-pipeline/* /home/ubuntu/dio_company_center/web_code/
+# 2. 安全搬運：從 Jenkins 自己的工作區，搬到剛剛開闢的內部門戶 /var/web_code
+#  這樣一搬，宿主機的實體 web_code 資料夾就會隔空同步收到檔案！
+cp -r /var/jenkins_home/workspace/dio-html-pipeline/* /var/web_code/
 
-# 3. 讓 Nginx 網頁貨櫃死死掛載在「純淨的 web_code」
+# 3. 啟動 Nginx：讓 Nginx 抱住宿主機實體的 web_code
 docker run -d \
   --name my-html-web \
   -p 80:80 \
